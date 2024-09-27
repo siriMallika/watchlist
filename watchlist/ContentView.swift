a1be59408cdeb3a29e682b13a5049263e0399f85
Mallika Sirivorajanyadee <Mallika.siri2536@gmail.com>
2024-09-27 16:45:17 +0700

    Update ContentView.swift

--- watchlist/ContentView.swift
+++ watchlist/ContentView.swift
@@ -7,17 +7,62 @@
 
 import SwiftUI
 
+struct Movie:Identifiable {
+    var id: UUID=UUID()
+    var title: String
+    var detail:String
+}
+
 struct ContentView: View {
+    
+    var movies: [Movie] = [Movie( title: "วิมานทุเรียน", detail: "หนึ่งในตัวละครเอกของภาพยนตร์เรื่อง วิมานหนาม ก็คือ บ้านและสวนทุเรียนหมอนทองคำเสก แม้ว่าในตัวเรื่องจะถูกเซ็ตเอาไว้ว่าเป็นจังหวัดแม่ฮ่องสอน แต่ในความเป็นจริง โลเคชั่นนี้ถูกถ่ายทำขึ้นที่ ไร่ตาเพ่ง ตำบลด่านชุมพล อำเภอบ่อไร่ จังหวัดตาก สวนทุเรียนที่มีความกว้างขวางกว่า 4,000 ไร่"), Movie( title: "TRANSFORMERS ONE (2024)", detail: "TRANSFORMERS ONE is the untold origin story of Optimus Prime and Megatron, better known as sworn enemies, but once were friends bonded like brothers who changed the fate of Cybertron forever. In the first-ever fully CG-animated Transformers movie, TRANSFORMERS ONE features a star-studded voice cast, including Chris Hemsworth, Brian Tyree Henry, Scarlett Johansson, Keegan-Michael Key, Steve Buscemi, with Laurence Fishburne, and Jon Hamm."),Movie(title: "A", detail: "Bbbbbb")]
+    
+    @State var showAlert:Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            List(movies){movie in
+                NavigationLink{
+                    DetailView(movie:movie)
+                }label: {
+                    //ใส่หน้าตาปุ่ม
+                    VStack (alignment: .leading){
+                      Text(movie.title)
+                      .font(.title3)
+                      .fontWeight(.semibold)
+                      Text(movie.detail)
+                       .font(.footnote)
+                       .foregroundStyle(.gray)
+                  }
+                }
+                
+            }
+            .listStyle(.plain)
+            .navigationTitle("Movie Watchlist")
+            .toolbar{
+                Button("Add"){
+                    showAlert = true
+                }
+            }
         }
-        .padding()
+        .alert("Add Movie", isPresented: $showAlert){
+            
+        }
     }
 }
+
+struct DetailView:View {
+    var movie: Movie
+    var body: some View {
+        Text(movie.title)
+            .font(.title3)
+            .fontWeight(.semibold)
+        Text(movie.detail)
+            .font(.footnote)
+            .foregroundStyle(.gray)
+    }
+}
+
 
 #Preview {
     ContentView()
