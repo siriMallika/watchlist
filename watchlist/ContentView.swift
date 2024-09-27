2652c1eda4fec900d64647629e7b7fdd4d2389d0
Mallika <Mallika.siri2536@gmail.com>
2024-09-27 14:55:15 +0700

    Initial Commit

---
+++ watchlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchlist
+//
+//  Created by admin2 on 27/9/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchlist/watchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchlistApp.swift
+//  watchlist
+//
+//  Created by admin2 on 27/9/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct watchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
